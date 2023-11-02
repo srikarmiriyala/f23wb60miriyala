f9eb8cd477b10463bd8fbe4075178edbca6f73c6
srikarmiriyala <srikarmiriyala@gmail.com>
2023-11-01 21:48:34 -0500

    added new files

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var vehicleRouter = require('./routes/vehicle');
+var chooseRouter = require('./routes/choose');
+var boaredRouter = require('./routes/board');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/vehicle', vehicleRouter);
+app.use('/choose', chooseRouter);
+app.use('/board', boaredRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
